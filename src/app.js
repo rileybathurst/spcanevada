bcad18140eab4ab5ec3c807ab8b85ee39bfb77fa
Riley Bathurst <riley@rileybathurst.com>
2019-11-16 15:09:22 -0800

    mobile size might be close

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import _ from 'lodash';
+
+let miniMenu = document.getElementById("minimenu"); // less complicated than a foreach or a loop with a class
+document.getElementById('mm').onclick = function changeContent() {
+    miniMenu.classList.toggle("minimenu-open");
+}
+
+// click outside the minimenu to remove it
+function remover() {
+    if (miniMenu.classList.contains('minimenu-open')) {
+        miniMenu.classList.remove("minimenu-open");
+    }
+}
