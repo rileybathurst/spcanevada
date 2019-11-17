8df2132e9193be960dc49aa7c361a955404eac73
Riley Bathurst <riley@rileybathurst.com>
2019-11-17 08:29:07 -0800

    friday night

--- src/app.js
+++ src/app.js
@@ -11,3 +11,59 @@ function remover() {
         miniMenu.classList.remove("minimenu-open");
     }
 }
+
+
+// Large margins when IO comes into play
+if ('IntersectionObserver' in window) {
+
+    let iopush = document.querySelectorAll('.io-push');
+
+    var numSteps = 20.0;
+
+    var pusher; // naming choice is too close to push
+    var prevRatio = 0.0;
+    var mT = "ratioem";
+
+    // Set things up.
+    window.addEventListener("load", function(event) {
+        pusher = document.querySelectorAll(".io-push");
+
+        createObserver();
+    }, false);
+
+    function createObserver() {
+        var observer;
+
+        var options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: buildThresholdList()
+        };
+
+        observer = new IntersectionObserver(handleIntersect, options);
+
+        pusher.forEach(pushers => {
+            observer.observe(pushers);
+        });
+    }
+
+    function buildThresholdList() {
+        var thresholds = [];
+        var numSteps = 100;
+
+        for (var i=1.0; i<=numSteps; i++) {
+            var ratio = i/numSteps;
+            thresholds.push(ratio);
+        }
+
+        thresholds.push(0);
+        return thresholds;
+    }
+
+    function handleIntersect(entries, observer) {
+        entries.forEach(function(entry) {
+            entry.target.style.marginTop = mT.replace("ratio", (entry.intersectionRatio*5));
+            prevRatio = entry.intersectionRatio;
+        });
+    }
+}
